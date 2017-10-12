d3943f7d8979ae6267a62ac043fb3c06abe1a24b
Tatsiana Yankovich <Tatsiana_Yankovich@epam.com>
2017-10-12 20:13:38 +0300

    Add files via upload

---
+++ lesson1.java
@@ -0,0 +1,9 @@
+package epam.training;
+
+public class lesson1 {
+
+	public static void main(String[] args) {
+		System.out.println("Tatsiana Yankovich\nN 186, room 411\n+375 33 663 53 79");
+	}
+
+}
