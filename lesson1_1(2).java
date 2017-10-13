7ab6b0429fce8d1fe7d4dcf21b65278c1ce706d8
Tatsiana Yankovich <Tatsiana_Yankovich@epam.com>
2017-10-13 16:02:26 +0300

    Delete lesson1_1(2).java

--- lesson1_1(2).java
+++
@@ -1,9 +0,0 @@
-package epam.training;
-
-public class lesson1 {
-
-	public static void main(String[] args) {
-		System.out.println("Tatsiana Yankovich\nN 186, room 411\n+375 33 663 53 79");
-	}
-
-}
