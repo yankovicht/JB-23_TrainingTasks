d6c19cbfa296ac0f635a31e9b63f5f66746da41d
Tatsiana Yankovich <Tatsiana_Yankovich@epam.com>
2017-10-16 17:34:55 +0300

    Add files via upload

---
+++ Lesson1_2/src/hometask/Task1.java
@@ -0,0 +1,32 @@
+package hometask;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		double a = 0, b = 0, c = 0, first, second;
+		sc = new Scanner(System.in);
+		System.out.print("Введите a: ");
+		if (sc.hasNextFloat()) {
+			a = sc.nextFloat();
+		}
+		System.out.print("Введите b: ");
+		if (sc.hasNextFloat()) {
+			b = sc.nextFloat();
+		}
+		if (a == b) {
+			System.out.print("Cкоро Новый год!");
+		} else {
+				System.out.print("Введите c: ");
+				if (sc.hasNextFloat()) {
+					c = sc.nextFloat();
+				}
+				first = a + b + c;
+				second = Math.pow(a,2)+Math.pow(b,2);
+				System.out.print(first+"\n"+second+"\n"+"Моя любимая футбольная команда");
+			}
+		}
+	}
