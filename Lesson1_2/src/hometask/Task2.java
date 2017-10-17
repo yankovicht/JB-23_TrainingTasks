958f1d7b663cb0674ea2c895682de21538368a6a
Tatsiana Yankovich <Tatsiana_Yankovich@epam.com>
2017-10-17 17:59:19 +0300

    Add files via upload

---
+++ Lesson1_2/src/hometask/Task2.java
@@ -0,0 +1,34 @@
+package hometask;
+
+import java.util.Scanner;
+
+public class Task2 {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		int n = 0, k = 0, tr = 0, mod = 0;
+		System.out.print("Введите количество чисел: ");
+		sc = new Scanner(System.in);
+		if (sc.hasNextInt()) {
+			n = sc.nextInt();
+		}
+		int[] array;
+		array = new int[n];
+		for (int i = 0; i < n; i++) {
+			System.out.print("Введите целое число: ");
+			if (sc.hasNextInt()) {
+				array[i] = sc.nextInt();
+				if (array[i] % 2 == 0) {
+					k++;
+				}
+				if (array[i] % 3 == 0) {
+					tr++;
+				}
+				if (Math.abs(array[i]) < 3) {
+					mod++;
+				}
+			}
+		}
+		System.out.println("\nЧётных " + k + ", кратных трём " + tr + ", меньше трёх по модулю " + mod);
+	}
+}
